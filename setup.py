c2078df37c3e6adcaa295babc4804a62186cd6c8
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2018-11-09 15:05:56 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AutoMunge_pkg",
+    version="1.51",
+    author="Nicholas Teague",
+    author_email="automunge@gmail.com",
+    description="A tool for automated data wrangling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Automunge/AutoMunge",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
+        "Operating System :: OS Independent",
+    ],
+)
