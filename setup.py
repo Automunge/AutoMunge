ae0c4909a0b304ac40e8e9ee91dbd57ec54933b9
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2018-12-02 19:41:08 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AutoMunge_pkg",
-    version="1.71",
+    version="1.72",
     author="Nicholas Teague",
     author_email="automunge@gmail.com",
     description="A tool for automated data wrangling",
