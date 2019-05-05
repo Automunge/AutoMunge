8c3663cd959a43252010c0f9c8b7cce589b1e3a3
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2019-05-05 17:00:33 -0500

    v1.800
    v1.800

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AutoMunge_pkg",
-    version="1.80",
+    version="1.800",
     author="Nicholas Teague",
     author_email="automunge@gmail.com",
     description="A tool for automated data wrangling",
