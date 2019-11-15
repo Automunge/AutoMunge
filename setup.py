c72a7ea434d1b5b1804d2d20f2b474cf4b95c452
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2019-11-15 15:15:58 -0500

    v2.85
    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Automunge",
-    version="2.84",
+    version="2.85",
     author="Nicholas Teague",
     author_email="pitg888@gmail.com",
     description="A tool for automated data wrangling",
