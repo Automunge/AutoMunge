33f1545e09f33238a17dab43441a8e0966e27f0c
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2020-01-30 21:25:05 -0500

    v3.21
    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Automunge",
-    version="3.20",
+    version="3.21",
     author="Nicholas Teague",
     author_email="pitg888@gmail.com",
     description="A tool for automated data wrangling",
