9121defe9f4dfdd4f1cf05cb714686b2510619c2
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2020-07-05 23:01:02 -0400

    v4.21
    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Automunge",
-    version="4.20",
+    version="4.21",
     author="Nicholas Teague",
     author_email="pitg888@gmail.com",
     description="platform for preparing tabular data for machine learning",
