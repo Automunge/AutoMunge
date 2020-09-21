d1b4da69c6827e75675df61e647c13d1c3035f79
Automunge <44011748+Automunge@users.noreply.github.com>
2020-09-21 18:52:19 -0400

    4.77
    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Automunge",
-    version="4.76",
+    version="4.77",
     author="Nicholas Teague",
     author_email="pitg888@gmail.com",
     description="platform for preparing tabular data for machine learning",
