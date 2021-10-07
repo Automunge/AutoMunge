1abce98bea8e902058315d89e2cb41fbac15cedb
Automunge <44011748+Automunge@users.noreply.github.com>
2021-10-06 20:48:12 -0400

    7.21
    update version number

--- Automunge/Automunger.py
+++ Automunge/Automunger.py
@@ -36686,7 +36686,7 @@ class AutoMunge:
     #note that we follow convention of using float equivalent strings as version numbers
     #to support backward compatibility checks
     #thus when reaching a round integer, the next version should be selected as int + 0.10 instead of 0.01
-    automungeversion = '7.20'
+    automungeversion = '7.21'
 #     application_number = random.randint(100000000000,999999999999)
 #     application_timestamp = dt.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
     version_combined = '_' + str(automungeversion) + '_' + str(application_number) + '_' \
